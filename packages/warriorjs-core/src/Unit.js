8038b058a4e9d4f4ab6e39c81d5e9c9627ec4cb9
Matias Olivera <moliverafreire@gmail.com>
2018-05-18 19:30:30 -0300

    Allow to create space from sensed space

--- packages/warriorjs-core/src/Unit.js
+++ packages/warriorjs-core/src/Unit.js
@@ -1,4 +1,5 @@
 import Logger from './Logger';
+import Space from './Space';
 
 /** Class representing a unit. */
 class Unit {
@@ -320,28 +321,32 @@ class Unit {
    * @returns {string} The relative direction of the stairs.
    */
   getDirectionOfStairs() {
-    return this.getDirectionOf(this.position.floor.getStairsSpace());
+    return this.position.getRelativeDirectionOf(
+      this.position.floor.getStairsSpace(),
+    );
   }
 
   /**
-   * Returns the direction of the given space with reference to this unit.
+   * Returns the direction of the given space, with reference to this unit.
    *
-   * @param {Space} space The space to get the direction of.
+   * @param {SensedSpace} sensedSpace The space to get the direction of.
    *
    * @returns {string} The relative direction of the space.
    */
-  getDirectionOf(space) {
+  getDirectionOf(sensedSpace) {
+    const space = Space.from(sensedSpace, this);
     return this.position.getRelativeDirectionOf(space);
   }
 
   /**
    * Returns the distance between the given space and this unit.
    *
-   * @param {Space} space The space to calculate the distance of.
+   * @param {SensedSpace} sensedSpace The space to calculate the distance of.
    *
    * @returns {number} The distance of the space.
    */
-  getDistanceOf(space) {
+  getDistanceOf(sensedSpace) {
+    const space = Space.from(sensedSpace, this);
     return this.position.getDistanceOf(space);
   }
 
